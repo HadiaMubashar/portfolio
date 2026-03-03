a2d7714ff30ef1b2578e44d746c3474207b7dd19
HadiaMubashar <111630382+HadiaMubashar@users.noreply.github.com>
2026-03-04 02:28:58 +0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,7 @@
 window.SITE = {
+  name: "Hadia Mubashar",
+  email: "hadiaj2002@gmail.com",
+  phone: "+92 3094177014",
   githubUsername: "HadiaMubashar",
-  linkedinUrl: "https://www.linkedin.com/in/hadia-mubashar",
-  email: "hadiaj2002@gmail.com"
+  linkedinUrl: "https://www.linkedin.com/in/hadia-mubashar"
 };
